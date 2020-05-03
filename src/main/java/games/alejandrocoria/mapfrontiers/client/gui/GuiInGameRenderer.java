ab627a67dd5855e94a1d26357a867559736bf3a1
Alejandro Coria <alejandrocoria2009@gmail.com>
2020-05-02 22:10:42 -0300

    some config options for the HUD

--- src/main/java/games/alejandrocoria/mapfrontiers/client/gui/GuiInGameRenderer.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/client/gui/GuiInGameRenderer.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import games.alejandrocoria.mapfrontiers.client.FrontierOverlay;
 import games.alejandrocoria.mapfrontiers.client.FrontiersOverlayManager;
+import games.alejandrocoria.mapfrontiers.common.ConfigData;
 import journeymap.client.io.ThemeLoader;
 import journeymap.client.ui.minimap.Shape;
 import journeymap.client.ui.theme.Theme;
@@ -49,16 +50,33 @@ public class GuiInGameRenderer {
             }
 
             ScaledResolution scaledresolution = new ScaledResolution(mc);
-            int width = scaledresolution.getScaledWidth();
-            int height = scaledresolution.getScaledHeight();
             int factor = scaledresolution.getScaleFactor();
 
             GlStateManager.pushMatrix();
             GlStateManager.scale(1.0 / factor, 1.0 / factor, 1.0);
 
-            int posX = mc.displayWidth - 250 - 11 * factor;
             String name1 = frontier.getName1();
             String name2 = frontier.getName2();
+
+            int hudWidth = 0;
+            int hudHeight = 0;
+
+            if (!StringUtils.isBlank(name1) || !StringUtils.isBlank(name2)) {
+                int name1Width = mc.fontRenderer.getStringWidth(name1) + 4;
+                int name2Width = mc.fontRenderer.getStringWidth(name2) + 4;
+                hudWidth = Math.max(name1Width, name2Width);
+                hudHeight += 24;
+            }
+
+            if (frontier.hasBanner()) {
+                hudWidth = Math.max(hudWidth, 22 * bannerScale + 4);
+                hudHeight += 40 * bannerScale + 4;
+            }
+
+
+            int posX = getXAnchorFromConfig() - getXOriginFromConfig(hudWidth);
+            int posY = getYAnchorFromConfig() - getYOriginFromConfig(hudHeight);
+
             int frameColor = 0xff000000;
             int textColor = 0xffffffff;
 
@@ -71,19 +89,15 @@ public class GuiInGameRenderer {
             }
 
             if (!StringUtils.isBlank(name1) || !StringUtils.isBlank(name2)) {
-                int name1Width = mc.fontRenderer.getStringWidth(name1) + 4;
-                int name2Width = mc.fontRenderer.getStringWidth(name2) + 4;
-                int nameWidth = Math.max(22 * bannerScale + 4, Math.max(name1Width, name2Width));
+                Gui.drawRect(posX, posY, posX + hudWidth, posY + 24, frameColor);
 
-                Gui.drawRect(posX - nameWidth / 2, 8, posX + nameWidth / 2, 32, frameColor);
-
-                frontierName1.setX(posX);
-                frontierName1.setY(10);
+                frontierName1.setX(posX + hudWidth / 2);
+                frontierName1.setY(posY + 2);
                 frontierName1.setText(name1);
                 frontierName1.setColor(textColor);
 
-                frontierName2.setX(posX);
-                frontierName2.setY(22);
+                frontierName2.setX(posX + hudWidth / 2);
+                frontierName2.setY(posY + 14);
                 frontierName2.setText(name2);
                 frontierName2.setColor(textColor);
 
@@ -92,12 +106,13 @@ public class GuiInGameRenderer {
             }
 
             if (frontier.hasBanner()) {
-                Gui.drawRect(posX - 11 * bannerScale - 2, 32, posX + 11 * bannerScale + 2, 36 + 40 * bannerScale, frameColor);
+                Gui.drawRect(posX + hudWidth / 2 - 11 * bannerScale - 2, posY + 24, posX + hudWidth / 2 + 11 * bannerScale + 2,
+                        posY + 28 + 40 * bannerScale, frameColor);
 
                 frontier.bindBannerTexture(mc);
                 GlStateManager.color(1.f, 1.f, 1.f);
-                Gui.drawModalRectWithCustomSizedTexture(posX - 11 * bannerScale, 34, 0, bannerScale, 22 * bannerScale,
-                        40 * bannerScale, 64 * bannerScale, 64 * bannerScale);
+                Gui.drawModalRectWithCustomSizedTexture(posX + hudWidth / 2 - 11 * bannerScale, posY + 26, 0, bannerScale,
+                        22 * bannerScale, 40 * bannerScale, 64 * bannerScale, 64 * bannerScale);
             }
 
             GlStateManager.popMatrix();
@@ -117,6 +132,100 @@ public class GuiInGameRenderer {
                 frontier = frontiersOverlayManager.getFrontierInPosition(player.dimension, lastPlayerPosition);
             }
         }
+    }
+
+    private int getXAnchorFromConfig() {
+        switch (ConfigData.hud.anchor) {
+        case ScreenTop:
+        case ScreenBottom:
+            return mc.displayWidth / 2;
+        case ScreenTopRight:
+        case ScreenRight:
+        case ScreenBottomRight:
+            return mc.displayWidth;
+        case ScreenBottomLeft:
+        case ScreenLeft:
+        case ScreenTopLeft:
+            return 0;
+        case Minimap:
+        case MinimapHorizontal:
+            // @Incomplete
+            return mc.displayWidth - 250;
+        case MinimapVertical:
+            return mc.displayWidth;
+        }
+
+        return 0;
+    }
+
+    private int getYAnchorFromConfig() {
+        switch (ConfigData.hud.anchor) {
+        case ScreenTopLeft:
+        case ScreenTop:
+        case ScreenTopRight:
+            return 0;
+        case ScreenBottomLeft:
+        case ScreenBottom:
+        case ScreenBottomRight:
+            return mc.displayHeight;
+        case ScreenRight:
+        case ScreenLeft:
+            return mc.displayHeight / 2;
+        case Minimap:
+        case MinimapVertical:
+            // @Incomplete
+            return 250;
+        case MinimapHorizontal:
+            return 0;
+        }
+
+        return 0;
+    }
+
+    private int getXOriginFromConfig(int hudWidth) {
+        switch (ConfigData.hud.anchor) {
+        case ScreenTop:
+        case ScreenBottom:
+            return hudWidth / 2;
+        case ScreenTopRight:
+        case ScreenRight:
+        case ScreenBottomRight:
+            return hudWidth;
+        case ScreenBottomLeft:
+        case ScreenLeft:
+        case ScreenTopLeft:
+            return 0;
+        case Minimap:
+        case MinimapHorizontal:
+        case MinimapVertical:
+            // @Incomplete
+            return hudWidth;
+        }
+
+        return 0;
+    }
+
+    private int getYOriginFromConfig(int hudHeight) {
+        switch (ConfigData.hud.anchor) {
+        case ScreenTopLeft:
+        case ScreenTop:
+        case ScreenTopRight:
+            return 0;
+        case ScreenBottomLeft:
+        case ScreenBottom:
+        case ScreenBottomRight:
+            return hudHeight;
+        case ScreenRight:
+        case ScreenLeft:
+            return hudHeight / 2;
+        case Minimap:
+        case MinimapHorizontal:
+        case MinimapVertical:
+            // @Incomplete
+            return 0;
+        }
+
+        return 0;
     }
 
     private static int getIntColor(Theme.ColorSpec colorSpec) {
--- src/main/java/games/alejandrocoria/mapfrontiers/common/ConfigData.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/common/ConfigData.java
@@ -12,6 +12,11 @@ public class ConfigData {
         Manual, Show, Hide
     }
 
+    public enum HUDAnchor {
+        ScreenTop, ScreenTopRight, ScreenRight, ScreenBottomRight, ScreenBottom, ScreenBottomLeft, ScreenLeft, ScreenTopLeft,
+        Minimap, MinimapHorizontal, MinimapVertical
+    };
+
     @Comment({ "If true, when a new frontier is created, the first vertex will automatically be added where the player is." })
     public static boolean addVertexToNewFrontier = true;
 
@@ -28,4 +33,21 @@ public class ConfigData {
     @Comment({ "Distance at which vertices are attached to nearby vertices." })
     @RangeInt(min = 0, max = 16)
     public static int snapDistance = 8;
+
+    public static HUDConfig hud = new HUDConfig();
+
+    public static class HUDConfig {
+        @Comment({
+                "Anchor point of the HUD. In the case of choosing the minimap as an anchor, its default position will be used as a reference in the coordinates." })
+        public HUDAnchor anchor = HUDAnchor.MinimapHorizontal;
+
+        @Comment({ "Position of the HUD relative to anchor." })
+        public Point position = new Point();
+    }
+
+
+    public static class Point {
+        public int x = 0;
+        public int y = 0;
+    }
 }
