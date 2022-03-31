98d7bb52a94525e1add86375e1e2fa382fc7cc5a
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-03-31 15:10:39 -0300

    updated version to 1.18.2-2.0.0beta3

--- src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
@@ -42,7 +42,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(MapFrontiers.MODID)
 public class MapFrontiers {
     public static final String MODID = "mapfrontiers";
-    public static final String VERSION = "1.18.2-2.0.0beta2";
+    public static final String VERSION = "1.18.2-2.0.0beta3";
     public static Logger LOGGER;
 
     private static FrontiersManager frontiersManager;
