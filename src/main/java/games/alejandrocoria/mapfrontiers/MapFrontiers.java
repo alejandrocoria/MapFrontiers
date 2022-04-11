13f49989e8eda0e581614d7dabd464d5075f63c3
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-04-11 15:16:30 -0300

    added filters on frontier list

--- src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
@@ -42,7 +42,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(MapFrontiers.MODID)
 public class MapFrontiers {
     public static final String MODID = "mapfrontiers";
-    public static final String VERSION = "1.17.1-2.0.0";
+    public static final String VERSION = "1.17.1-2.1.0";
     public static Logger LOGGER;
 
     private static FrontiersManager frontiersManager;
