bb7a1ee9e3e5b5372f799b10082728cb97fb0ea8
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-05-26 15:30:55 -0300

    updated version to 1.18.2-2.1.5-beta1

--- src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
@@ -40,7 +40,7 @@ import java.util.ArrayList;
 @Mod(MapFrontiers.MODID)
 public class MapFrontiers {
     public static final String MODID = "mapfrontiers";
-    public static final String VERSION = "1.18.2-2.1.4";
+    public static final String VERSION = "1.18.2-2.1.5-beta1";
     public static Logger LOGGER;
 
     private static FrontiersManager frontiersManager;
