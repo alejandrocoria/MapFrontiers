b6b01f7af0624fd1377e9a57aeba284f566108af
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-07-10 14:06:01 -0300

    fixed crash when entering or exiting a frontier with announcement in chat caused by wrong import

--- src/main/java/games/alejandrocoria/mapfrontiers/client/ClientProxy.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/client/ClientProxy.java
@@ -36,7 +36,7 @@ import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import org.codehaus.plexus.util.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.lwjgl.glfw.GLFW;
 
 import javax.annotation.Nullable;
