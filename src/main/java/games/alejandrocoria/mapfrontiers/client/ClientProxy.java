c4d362482a87899c0643798ebda315dc4b078112
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-07-28 17:21:58 -0300

    fixed: creating a personal frontier creates a global frontier

--- src/main/java/games/alejandrocoria/mapfrontiers/client/ClientProxy.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/client/ClientProxy.java
@@ -215,7 +215,7 @@ public class ClientProxy {
         }
 
         if (personalFrontiersOverlayManager == null) {
-            personalFrontiersOverlayManager = new FrontiersOverlayManager(jmAPI, false);
+            personalFrontiersOverlayManager = new FrontiersOverlayManager(jmAPI, true);
         }
     }
 
