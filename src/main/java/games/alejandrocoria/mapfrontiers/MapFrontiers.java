a14efa5da18a0ef363ab572d39c0a42190765aa4
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-09-20 19:35:32 -0300

    updated version

--- src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
@@ -37,7 +37,7 @@ import java.util.ArrayList;
 @Mod(MapFrontiers.MODID)
 public class MapFrontiers {
     public static final String MODID = "mapfrontiers";
-    public static final String VERSION = "1.19.1-2.3.4-beta1";
+    public static final String VERSION = "1.19.1-2.4.0-beta1";
     public static Logger LOGGER;
 
     private static FrontiersManager frontiersManager;
