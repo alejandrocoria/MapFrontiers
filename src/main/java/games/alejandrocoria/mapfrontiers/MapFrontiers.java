be2ca50e82be6cc8cf852c54170eb61f4af3992c
Alejandro Coria <alejandrocoria2009@gmail.com>
2022-09-20 19:35:41 -0300

    updated journeymap version

--- src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/MapFrontiers.java
@@ -37,7 +37,7 @@ import java.util.ArrayList;
 @Mod(MapFrontiers.MODID)
 public class MapFrontiers {
     public static final String MODID = "mapfrontiers";
-    public static final String VERSION = "1.18.2-2.3.4";
+    public static final String VERSION = "1.18.2-2.4.0-beta1";
     public static Logger LOGGER;
 
     private static FrontiersManager frontiersManager;
