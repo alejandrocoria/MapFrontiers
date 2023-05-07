682eb9e9803492cccbd0e0d12a785c8dc3509d1c
Alejandro Coria <alejandrocoria2009@gmail.com>
2023-05-07 16:12:32 -0300

    copy and paste; save filter options; undo and redo

--- src/main/java/games/alejandrocoria/mapfrontiers/client/gui/GuiButtonIcon.java
+++ src/main/java/games/alejandrocoria/mapfrontiers/client/gui/GuiButtonIcon.java
@@ -17,26 +17,41 @@ import javax.annotation.ParametersAreNonnullByDefault;
 @Environment(EnvType.CLIENT)
 public class GuiButtonIcon extends Button {
     private static final ResourceLocation texture = new ResourceLocation(MapFrontiers.MODID + ":textures/gui/buttons.png");
-    private static final int textureSizeX = 26;
-    private static final int textureSizeY = 26;
+    private static final int textureSizeX = 34;
+    private static final int textureSizeY = 111;
 
     public enum Type {
-        Add, Remove
+        Add       ( 0,  0, 13,  0, 13, 13),
+        Remove    ( 0, 13, 13,  0, 13, 13),
+        Copy      ( 0, 26, 17, 26, 17, 17),
+        Paste     ( 0, 43, 17, 43, 17, 17),
+        ArrowUp   ( 0, 60,  8, 60,  8, 17),
+        ArrowDown (18, 60, 26, 60,  8, 17),
+        Undo      ( 0, 77, 17, 77, 17, 17),
+        Redo      ( 0, 94, 17, 94, 17, 17);
+
+        final int texX;
+        final int texY;
+        final int texHoverX;
+        final int texHoverY;
+        final int width;
+        final int height;
+
+        Type(int texX, int texY, int texHoverX, int texHoverY, int width, int height) {
+            this.texX = texX;
+            this.texY = texY;
+            this.texHoverX = texHoverX;
+            this.texHoverY = texHoverY;
+            this.width = width;
+            this.height = height;
+        }
     }
 
-    private int texY;
+    private Type type;
 
     public GuiButtonIcon(int x, int y, Type type, Button.OnPress pressedAction) {
-        super(x, y, 13, 13, Component.empty(), pressedAction);
-
-        switch (type) {
-            case Add:
-                texY = 0;
-                break;
-            case Remove:
-                texY = 13;
-                break;
-        }
+        super(x, y, type.width, type.height, Component.empty(), pressedAction);
+        this.type = type;
     }
 
     @Override
@@ -50,11 +65,10 @@ public class GuiButtonIcon extends Button {
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderTexture(0, texture);
 
-        int texX = 0;
         if (isHovered) {
-            texX = 13;
+            blit(matrixStack, x, y, type.texHoverX, type.texHoverY, width, height, textureSizeX, textureSizeY);
+        } else {
+            blit(matrixStack, x, y, type.texX, type.texY, width, height, textureSizeX, textureSizeY);
         }
-
-        blit(matrixStack, x, y, texX, texY, width, height, textureSizeX, textureSizeY);
     }
 }
