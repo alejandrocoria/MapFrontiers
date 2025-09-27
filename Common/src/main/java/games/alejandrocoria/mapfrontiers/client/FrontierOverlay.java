bee84836fd9b7c0e6818d97c74398068eb11936a
Alejandro Coria <alejandrocoria2009@gmail.com>
2025-09-27 16:32:26 -0300

    fixed snapping

--- Common/src/main/java/games/alejandrocoria/mapfrontiers/client/FrontierOverlay.java
+++ Common/src/main/java/games/alejandrocoria/mapfrontiers/client/FrontierOverlay.java
@@ -774,7 +774,8 @@ public class FrontierOverlay extends FrontierData {
     }
 
     private BlockPos snapVertex(BlockPos vertex, float snapDistance) {
-        BlockPos closest = vertex.atY(70);
+        vertex = vertex.atY(70);
+        BlockPos closest = vertex;
         double closestDistance = snapDistance * snapDistance;
 
         for (FrontierOverlay frontier : MapFrontiersClient.getFrontiersOverlayManager(true).getAllFrontiers(dimension)) {
@@ -782,10 +783,13 @@ public class FrontierOverlay extends FrontierData {
                 continue;
             }
 
-            BlockPos v = frontier.getClosestVertex(closest, closestDistance);
+            BlockPos v = frontier.getClosestVertex(vertex, closestDistance);
             if (v != null) {
-                closest = v;
-                closestDistance = v.distSqr(vertex);
+                double dist = v.distSqr(vertex);
+                if (dist <= closestDistance) {
+                    closest = v;
+                    closestDistance = dist;
+                }
             }
         }
 
@@ -794,10 +798,13 @@ public class FrontierOverlay extends FrontierData {
                 continue;
             }
 
-            BlockPos v = frontier.getClosestVertex(closest, closestDistance);
+            BlockPos v = frontier.getClosestVertex(vertex, closestDistance);
             if (v != null) {
-                closest = v;
-                closestDistance = v.distSqr(vertex);
+                double dist = v.distSqr(vertex);
+                if (dist <= closestDistance) {
+                    closest = v;
+                    closestDistance = dist;
+                }
             }
         }
 
