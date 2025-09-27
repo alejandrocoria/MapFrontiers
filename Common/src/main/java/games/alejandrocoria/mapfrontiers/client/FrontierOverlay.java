395835477a66fef1b7d218919f64735a112f203c
Alejandro Coria <alejandrocoria2009@gmail.com>
2025-09-27 16:32:26 -0300

    fixed snapping

--- Common/src/main/java/games/alejandrocoria/mapfrontiers/client/FrontierOverlay.java
+++ Common/src/main/java/games/alejandrocoria/mapfrontiers/client/FrontierOverlay.java
@@ -767,7 +767,8 @@ public class FrontierOverlay extends FrontierData {
     }
 
     private BlockPos snapVertex(BlockPos vertex, float snapDistance) {
-        BlockPos closest = vertex.atY(70);
+        vertex = vertex.atY(70);
+        BlockPos closest = vertex;
         double closestDistance = snapDistance * snapDistance;
 
         for (FrontierOverlay frontier : MapFrontiersClient.getFrontiersOverlayManager(true).getAllFrontiers(dimension)) {
@@ -775,10 +776,13 @@ public class FrontierOverlay extends FrontierData {
                 continue;
             }
 
-            BlockPos v = frontier.getClosestVertex(closest, closestDistance);
+            BlockPos v = frontier.getClosestVertex(vertex, closestDistance);
             if (v != null) {
-                closest = v;
-                closestDistance = v.distSqr(vertex);
+                double dist = v.distSqr(vertex);
+                if (dist <= closestDistance) {
+                    closest = v;
+                    closestDistance = dist;
+                }
             }
         }
 
@@ -787,10 +791,13 @@ public class FrontierOverlay extends FrontierData {
                 continue;
             }
 
-            BlockPos v = frontier.getClosestVertex(closest, closestDistance);
+            BlockPos v = frontier.getClosestVertex(vertex, closestDistance);
             if (v != null) {
-                closest = v;
-                closestDistance = v.distSqr(vertex);
+                double dist = v.distSqr(vertex);
+                if (dist <= closestDistance) {
+                    closest = v;
+                    closestDistance = dist;
+                }
             }
         }
 
--- doc/changelog.md
+++ doc/changelog.md
@@ -1,3 +1,6 @@
+**Beta 16:**
+* Fixed: Snapping acting weird.
+ 
 **Beta 15:**
 * Fixed: [Forge] HUD not drawing.
  
