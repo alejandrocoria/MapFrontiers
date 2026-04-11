7e084075f5486e4f4bc62111940a389492e18985
Alejandro Coria <alejandrocoria2009@gmail.com>
2026-04-11 14:16:01 -0300

    preview phase 7

--- Common/src/main/java/games/alejandrocoria/mapfrontiers/client/gui/component/PreviewFrontiersWidget.java
+++ Common/src/main/java/games/alejandrocoria/mapfrontiers/client/gui/component/PreviewFrontiersWidget.java
@@ -12,6 +12,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
 import javax.annotation.ParametersAreNonnullByDefault;
@@ -21,6 +22,7 @@ import java.util.List;
 @ParametersAreNonnullByDefault
 public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
     private static final int SIZE = 420;
+    private static final ResourceKey<Level> OVERWORLD = ResourceKey.create(Registries.DIMENSION, Identifier.withDefaultNamespace("overworld"));
 
     private final FrontierPreviewPanel previewPanel;
     private final List<FrontierOverlay> previewFrontiers = new ArrayList<>();
@@ -32,40 +34,9 @@ public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
 
         SettingsUser owner = new SettingsUser();
         owner.username = "Player";
-        FrontierData frontierData = new FrontierData();
-        frontierData.setOwner(owner);
-        frontierData.setName1("Preview");
-        frontierData.setName2("Frontier");
-        frontierData.setColor(0xFFAACC60);
-        PreviewFrontierHelper.setPreviewBanner(frontierData);
-        frontierData.setDimension(ResourceKey.create(Registries.DIMENSION, Identifier.withDefaultNamespace("overworld")));
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenDay, true);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenName, true);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenOwner, true);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenBanner, true);
-        frontierData.addVertex(new BlockPos(10, 70, 10));
-        frontierData.addVertex(new BlockPos(10, 70, 410));
-        frontierData.addVertex(new BlockPos(270, 70, 410));
-        frontierData.addVertex(new BlockPos(270, 70, 10));
-        previewFrontiers.add(new FrontierOverlay(frontierData, null));
-
-        frontierData = new FrontierData();
-        frontierData.setOwner(owner);
-        frontierData.setName1("Long name");
-        frontierData.setName2("12345678901234567");
-        frontierData.setColor(0xFFA0A0FF);
-        frontierData.setDimension(ResourceKey.create(Registries.DIMENSION, Identifier.withDefaultNamespace("overworld")));
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenDay, true);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenName, true);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenOwner, false);
-        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenBanner, false);
-        frontierData.addVertex(new BlockPos(240, 70, 280));
-        frontierData.addVertex(new BlockPos(300, 70, 245));
-        frontierData.addVertex(new BlockPos(360, 70, 280));
-        frontierData.addVertex(new BlockPos(360, 70, 350));
-        frontierData.addVertex(new BlockPos(300, 70, 385));
-        frontierData.addVertex(new BlockPos(240, 70, 350));
-        previewFrontiers.add(new FrontierOverlay(frontierData, null));
+        previewFrontiers.add(new FrontierOverlay(createBannerFrontier(owner), null));
+        previewFrontiers.add(new FrontierOverlay(createLongNameFrontier(owner), null));
+        previewFrontiers.add(new FrontierOverlay(createPathFrontier(owner), null));
 
         configUpdated();
     }
@@ -92,5 +63,62 @@ public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
     @Nullable
     public ComponentPath nextFocusPath(FocusNavigationEvent event) {
         return null;
+    }
+
+    private static FrontierData createBannerFrontier(SettingsUser owner) {
+        FrontierData frontierData = new FrontierData();
+        frontierData.setOwner(owner);
+        frontierData.setName1("Preview");
+        frontierData.setName2("Frontier");
+        frontierData.setColor(0xFFAACC60);
+        PreviewFrontierHelper.setPreviewBanner(frontierData);
+        frontierData.setDimension(OVERWORLD);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenDay, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenName, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenOwner, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenBanner, true);
+        frontierData.addVertex(new BlockPos(10, 70, 10));
+        frontierData.addVertex(new BlockPos(10, 70, 410));
+        frontierData.addVertex(new BlockPos(270, 70, 410));
+        frontierData.addVertex(new BlockPos(270, 70, 10));
+        return frontierData;
+    }
+
+    private static FrontierData createLongNameFrontier(SettingsUser owner) {
+        FrontierData frontierData = new FrontierData();
+        frontierData.setOwner(owner);
+        frontierData.setName1("Long name");
+        frontierData.setName2("12345678901234567");
+        frontierData.setColor(0xFFA0A0FF);
+        frontierData.setDimension(OVERWORLD);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenDay, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenName, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenOwner, false);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenBanner, false);
+        frontierData.addVertex(new BlockPos(240, 70, 280));
+        frontierData.addVertex(new BlockPos(300, 70, 245));
+        frontierData.addVertex(new BlockPos(360, 70, 280));
+        frontierData.addVertex(new BlockPos(360, 70, 350));
+        frontierData.addVertex(new BlockPos(300, 70, 385));
+        frontierData.addVertex(new BlockPos(240, 70, 350));
+        return frontierData;
+    }
+
+    private static FrontierData createPathFrontier(SettingsUser owner) {
+        FrontierData frontierData = new FrontierData();
+        frontierData.setMode(FrontierData.Mode.Path);
+        frontierData.setOwner(owner);
+        frontierData.setName1("Path");
+        frontierData.setName2("Preview");
+        frontierData.setColor(0xFFFFC04D);
+        frontierData.setDimension(OVERWORLD);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenDay, true);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenName, false);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenOwner, false);
+        frontierData.setVisibility(FrontierData.VisibilityData.Visibility.FullscreenBanner, false);
+        frontierData.addPoint(new BlockPos(305, 70, 40));
+        frontierData.addPoint(new BlockPos(390, 70, 95));
+        frontierData.addPoint(new BlockPos(320, 70, 170));
+        return frontierData;
     }
 }
