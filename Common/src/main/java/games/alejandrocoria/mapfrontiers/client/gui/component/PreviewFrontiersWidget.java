bfe73ac9d7034f6ded52536c412d577e954fba76
Alejandro Coria <alejandrocoria2009@gmail.com>
2026-04-11 14:09:58 -0300

    preview phase 6

--- Common/src/main/java/games/alejandrocoria/mapfrontiers/client/gui/component/PreviewFrontiersWidget.java
+++ Common/src/main/java/games/alejandrocoria/mapfrontiers/client/gui/component/PreviewFrontiersWidget.java
@@ -1,17 +1,12 @@
 package games.alejandrocoria.mapfrontiers.client.gui.component;
 
-import games.alejandrocoria.mapfrontiers.MapFrontiers;
 import games.alejandrocoria.mapfrontiers.client.frontier.FrontierOverlay;
-import games.alejandrocoria.mapfrontiers.client.gui.ColorConstants;
 import games.alejandrocoria.mapfrontiers.common.frontier.FrontierData;
 import games.alejandrocoria.mapfrontiers.common.settings.SettingsUser;
-import games.alejandrocoria.mapfrontiers.platform.Services;
-import games.alejandrocoria.mapfrontiers.platform.services.IJourneyMapHelper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.ComponentPath;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.navigation.FocusNavigationEvent;
-import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
@@ -25,15 +20,15 @@ import java.util.List;
 
 @ParametersAreNonnullByDefault
 public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
-    private static final Identifier backgroundTexture = Identifier.fromNamespaceAndPath(MapFrontiers.MODID, "textures/gui/frontier_preview_bg.png");
     private static final int SIZE = 420;
 
-    private final IJourneyMapHelper.ICustomPreviewRenderer customPreviewRenderer;
+    private final FrontierPreviewPanel previewPanel;
     private final List<FrontierOverlay> previewFrontiers = new ArrayList<>();
     private float scaleFactor = 1;
 
     public PreviewFrontiersWidget() {
         super(0, 0, SIZE, SIZE, Component.empty());
+        previewPanel = new FrontierPreviewPanel();
 
         SettingsUser owner = new SettingsUser();
         owner.username = "Player";
@@ -72,16 +67,11 @@ public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
         frontierData.addVertex(new BlockPos(240, 70, 350));
         previewFrontiers.add(new FrontierOverlay(frontierData, null));
 
-        customPreviewRenderer = Services.JOURNEYMAP.createCustomPreviewRenderer();
         configUpdated();
     }
 
     public void configUpdated() {
-        for (FrontierOverlay frontierOverlay : previewFrontiers) {
-            frontierOverlay.recalculateOverlays();
-        }
-
-        customPreviewRenderer.setFrontiers(previewFrontiers);
+        previewPanel.recalculateAndSetFrontiers(previewFrontiers);
     }
 
     public void setScaleFactor(float scaleFactor) {
@@ -90,18 +80,13 @@ public class PreviewFrontiersWidget extends AbstractWidgetNoNarration {
         setWidth((int) (SIZE / guiScale));
         setHeight((int) (SIZE / guiScale));
 
-        customPreviewRenderer.setFrontiers(previewFrontiers);
+        previewPanel.refreshRenderer();
     }
 
     @Override
     protected void extractWidgetRenderState(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float partialTicks) {
-        graphics.blit(RenderPipelines.GUI_TEXTURED, backgroundTexture, getX(), getY(), 0, 0, getWidth(), getHeight(), SIZE, SIZE, SIZE, SIZE);
-        graphics.horizontalLine(getX(), getX() + getWidth() - 1, getY(), ColorConstants.OPTION_BORDER);
-        graphics.horizontalLine(getX(), getX() + getWidth() - 1, getY() + getHeight() - 1, ColorConstants.OPTION_BORDER);
-        graphics.verticalLine(getX(), getY(), getY() + getHeight() - 1, ColorConstants.OPTION_BORDER);
-        graphics.verticalLine(getX() + getWidth() - 1, getY(), getY() + getHeight() - 1, ColorConstants.OPTION_BORDER);
-
-        customPreviewRenderer.draw(graphics, Minecraft.getInstance().renderBuffers().bufferSource(), getX(), getY(), SIZE, scaleFactor);
+        previewPanel.drawPanel(graphics, getX(), getY(), getWidth(), getHeight());
+        previewPanel.drawPreview(graphics, getX(), getY(), SIZE, scaleFactor);
     }
 
     @Nullable
