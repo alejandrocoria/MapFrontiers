2d569f6cd1f8e5696a97ddb4284a9bda9c24960f
Alejandro Coria <alejandrocoria2009@gmail.com>
2026-07-05 21:00:34 -0300

    fix forge client tick event

--- Forge/src/main/java/games/alejandrocoria/mapfrontiers/client/MapFrontiersClientForge.java
+++ Forge/src/main/java/games/alejandrocoria/mapfrontiers/client/MapFrontiersClientForge.java
@@ -38,8 +38,10 @@ public class MapFrontiersClientForge extends MapFrontiersClient {
         }
     }
 
-    public static void onClientTickPre(TickEvent.ClientTickEvent.Pre event) {
-        ClientGlobalEvents.postClientTickEvent(Minecraft.getInstance());
+    public static void onClientTickPre(TickEvent.ClientTickEvent event) {
+        if (event.phase == TickEvent.Phase.START) {
+            ClientGlobalEvents.postClientTickEvent(Minecraft.getInstance());
+        }
     }
 
     public static void onRegisterGuiOverlays(RegisterGuiOverlaysEvent event) {
