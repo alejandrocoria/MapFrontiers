0f0e50a122f54f7d86171ad6169084ea463fde4a
Alejandro Coria <alejandrocoria2009@gmail.com>
2026-07-06 15:52:00 -0300

    fix forge production mixin remapping

--- Common/src/main/java/games/alejandrocoria/mapfrontiers/mixin/client/EditBoxMixin.java
+++ Common/src/main/java/games/alejandrocoria/mapfrontiers/mixin/client/EditBoxMixin.java
@@ -4,40 +4,38 @@ import games.alejandrocoria.mapfrontiers.client.gui.component.textbox.TextBox;
 import games.alejandrocoria.mapfrontiers.client.gui.component.textbox.TextBoxDouble;
 import games.alejandrocoria.mapfrontiers.client.gui.component.textbox.TextBoxInt;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.GuiGraphics;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(EditBox.class)
 public class EditBoxMixin {
-    @ModifyArgs(method = "renderWidget", at = @At(value = "INVOKE",
+    @Redirect(method = "renderWidget", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V", ordinal = 0), require = 0)
-    private void mapfrontiers$fitBorderToWidgetBounds(Args args) {
+    private void mapfrontiers$fitBorderToWidgetBounds(GuiGraphics graphics, int minX, int minY, int maxX, int maxY, int color) {
         if (!mapfrontiers$isMapFrontiersTextBox()) {
+            graphics.fill(minX, minY, maxX, maxY, color);
             return;
         }
 
         EditBox editBox = (EditBox) (Object) this;
-        args.set(0, editBox.getX());
-        args.set(1, editBox.getY());
-        args.set(2, editBox.getX() + editBox.getWidth());
-        args.set(3, editBox.getY() + editBox.getHeight());
+        graphics.fill(editBox.getX(), editBox.getY(), editBox.getX() + editBox.getWidth(), editBox.getY() + editBox.getHeight(),
+                color);
     }
 
-    @ModifyArgs(method = "renderWidget", at = @At(value = "INVOKE",
+    @Redirect(method = "renderWidget", at = @At(value = "INVOKE",
             target = "Lnet/minecraft/client/gui/GuiGraphics;fill(IIIII)V", ordinal = 1), require = 0)
-    private void mapfrontiers$fitBackgroundInsideBorder(Args args) {
+    private void mapfrontiers$fitBackgroundInsideBorder(GuiGraphics graphics, int minX, int minY, int maxX, int maxY, int color) {
         if (!mapfrontiers$isMapFrontiersTextBox()) {
+            graphics.fill(minX, minY, maxX, maxY, color);
             return;
         }
 
         EditBox editBox = (EditBox) (Object) this;
-        args.set(0, editBox.getX() + 1);
-        args.set(1, editBox.getY() + 1);
-        args.set(2, editBox.getX() + editBox.getWidth() - 1);
-        args.set(3, editBox.getY() + editBox.getHeight() - 1);
+        graphics.fill(editBox.getX() + 1, editBox.getY() + 1, editBox.getX() + editBox.getWidth() - 1,
+                editBox.getY() + editBox.getHeight() - 1, color);
     }
 
     @ModifyVariable(method = "renderWidget", at = @At("STORE"), index = 12, require = 0)
